720b2d21c6c1c85d6c2b2181f11fc8c765dee477
romanchom <romanchom@gmail.com>
2014-10-01 15:43:08 +0200

    Initial commit

---
+++ Assets/Ship.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ship : MonoBehaviour {
+	public float thrustMultiplier;
+
+	float thrust = 0;
+	void Start() {
+		Screen.lockCursor = true;
+		Screen.showCursor = false;
+	}
+
+	void FixedUpdate() {
+		Vector3 mouse = new Vector3();
+		mouse.x = -Input.GetAxis("Mouse Y");
+		mouse.y = Input.GetAxis("Mouse X");
+		Quaternion rot = Quaternion.Euler(mouse * 3);
+		transform.localRotation *= rot;
+
+		thrust += Input.GetAxis("Mouse ScrollWheel") * 1.0f;
+		thrust = Mathf.Clamp01(thrust);
+		rigidbody.velocity += transform.forward * thrust * thrustMultiplier * Time.fixedDeltaTime;
+	}
+
+	void OnGUI() {
+		GUILayout.BeginVertical();
+		GUILayout.Label(thrust.ToString());
+		GUILayout.EndVertical();
+	}
+}
