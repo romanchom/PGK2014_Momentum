2a53dda0ed2fd68a32dc8d662419c1937e2aa284
romanchom <romanchom@gmail.com>
2014-10-01 23:28:44 +0200

    Generowanie torów na potrzeby testów. Particle i takie tam.

--- Assets/Ship.cs
+++ Assets/Ship.cs
@@ -5,20 +5,24 @@ public class Ship : MonoBehaviour {
 	public float thrustMultiplier;
 
 	float thrust = 0;
-	void Start() {
+	/*void Start() {
 		Screen.lockCursor = true;
 		Screen.showCursor = false;
-	}
+	}//*/
 
 	void FixedUpdate() {
 		Vector3 mouse = new Vector3();
 		mouse.x = -Input.GetAxis("Mouse Y");
 		mouse.y = Input.GetAxis("Mouse X");
-		Quaternion rot = Quaternion.Euler(mouse * 3);
+
+		mouse.x = Input.GetAxis("Y");
+		mouse.y = Input.GetAxis("X");
+		Quaternion rot = Quaternion.Euler(mouse);
 		transform.localRotation *= rot;
 
-		thrust += Input.GetAxis("Mouse ScrollWheel") * 1.0f;
-		thrust = Mathf.Clamp01(thrust);
+		//thrust += Input.GetAxis("Mouse ScrollWheel") * 1.0f;
+		//thrust = Mathf.Clamp01(thrust);
+		thrust = -Input.GetAxis("Thrust");
 		rigidbody.velocity += transform.forward * thrust * thrustMultiplier * Time.fixedDeltaTime;
 	}
 
